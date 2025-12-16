cd5ed0467929ddcf3caf530f1e49a55666aa2c3c
Adrian Glasmyre <aglasmyre@gmail.com>
2025-12-16 18:00:26 -0500

    added CTA

---
+++ src/content/servicesList.ts
@@ -0,0 +1,74 @@
+export const servicesList = [
+  {
+    service: "Website & Landing Page Redesigns",
+  },
+  {
+    service: "Responsive Design",
+  },
+  {
+    service: "Social Media Design",
+  },
+  {
+    service: "PPC Ad Design",
+  },
+  {
+    service: "Branding",
+  },
+  {
+    service: "Amazon Graphics",
+  },
+  {
+    service: "UI/UX Design",
+  },
+  {
+    service: "A/B Testing",
+  },
+  {
+    service: "Micrographic & Conversion Assets",
+  },
+];
+
+export const toolsList = [
+  {
+    tool: "Figma",
+  },
+  {
+    tool: "Photoshop",
+  },
+  {
+    tool: "Illustrator",
+  },
+  {
+    tool: "Canva",
+  },
+  {
+    tool: "AfterEffects",
+  },
+  {
+    tool: "The Brief",
+  },
+  {
+    tool: "Affinity",
+  },
+  {
+    tool: "Wordpress",
+  },
+  {
+    tool: "Shopify",
+  },
+  {
+    tool: "HubSpot",
+  },
+  {
+    tool: "OmniConvert",
+  },
+  {
+    tool: "HTML/CSS",
+  },
+  {
+    tool: "PHP",
+  },
+  {
+    tool: "Astro",
+  },
+];
