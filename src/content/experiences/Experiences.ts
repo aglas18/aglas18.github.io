cd1b174a78e3153605652a8bcd1f19a9d94268a6
Adrian Glasmyre <aglasmyre@gmail.com>
2025-12-16 15:09:09 -0500

    added accordion and testimonials

---
+++ src/content/experiences/Experiences.ts
@@ -0,0 +1,73 @@
+export const experienceList = [
+  {
+    date: "Sept 2021 - Present",
+    company: "WebFX",
+  },
+  {
+    date: "Jan 2019 - May 2022",
+    company: "Lebanon Valley College",
+  },
+  {
+    date: "Aug 2020 - Mar 2021",
+    company: "Stop Modern Day Slavery",
+  },
+];
+
+export const jobList = [
+  {
+    company: "WebFX",
+    date: "Jun 2024 - Present",
+    title: "Associate Lead Web Designer",
+    content:
+      "Lead website redesigns, landing pages, and digital marketing projects, collaborating with developers and project managers to ensure designs are implemented accurately.",
+  },
+  {
+    company: "WebFX",
+    date: "May 2023 - Jun 2024",
+    title: "Web Designer",
+    content:
+      "Designed landing pages, responsive web and marketing assets, social media graphics, micrographics, conversion-focused assets, and longform blog layouts.",
+  },
+  {
+    company: "WebFX",
+    date: "May 2022 - May 2023",
+    title: "Jr. Web designer",
+    content:
+      "Built and implemented designs across multiple CMSs and page builders, including WordPress (with Advanced Custom Fields), Shopify, BigCommerce, HubSpot, Elementor, Divi, and Avada, ensuring responsive, high-quality digital experiences.",
+  },
+  {
+    company: "WebFX",
+    date: "sept 2021 - may 2022",
+    title: "Online graphics & web design intern",
+    content:
+      "Lead website redesigns, landing pages, and digital marketing projects, collaborating with developers and project managers to ensure designs are implemented accurately.",
+  },
+  {
+    company: "Lebanon Valley College",
+    date: "Jun 2021 - May 2022",
+    title: "Student Graphic Designer",
+    content:
+      "Designed marketing and promotional materials (including flyers, banners, social media content, and event visuals) for the Breen Center, collaborating with the Student Marketing Intern, Student Photographer, and Assistant Marketing Director to ensure alignment with LVC's branding guidelines.",
+  },
+  {
+    company: "Lebanon Valley College",
+    date: "Aug 2019 - May 2021",
+    title: "Career Peer",
+    content:
+      "Guided students in crafting polished resumes, cover letters, and exploring career pathways, while delivering engaging presentations and workshops to enhance their professional development and communication skills.",
+  },
+  {
+    company: "Lebanon Valley College",
+    date: "Jan 2019 - Mar 2020",
+    title: "Gallery Attendant",
+    content:
+      "Provided attentive customer service to gallery visitors, maintained accurate records of guest activity and museum conditions, and contributed to the visual presentation and overall experience of the gallery space.",
+  },
+  {
+    company: "Stop Modern Day Slavery",
+    date: "Aug 2020 - Mar 2021",
+    title: "Graphic Design Intern",
+    content:
+      "Designed and produced compelling graphics for the organization's website and social media channels, while creating custom illustrated stickers for fundraising and promotional initiatives, enhancing brand visibility and audience engagement.",
+  },
+];
---
+++ src/content/testimonials/TestimonialList.ts
@@ -0,0 +1,17 @@
+export const testimonialList = [
+  {
+    testimonial: `"Sometimes you work with someone and think to yourself, "They just get it." That's exactly how I feel every time I work with Adrian! Adrian, cheers to you for consistently finding creative ways to wow both me and our clients, and for always being so quick and thorough with edits. Truly appreciate you."`,
+    name: "Jordan Evans",
+    title: "Project Manager, WebFX",
+  },
+  {
+    testimonial: `"Huge thanks to Adrian for tackling tasks so quickly and always coming through with fresh ideas. Her recommendations make it easy to deliver exactly what the client wanted. Really appreciate all that you do, Adrian!"`,
+    name: "Jazlyn Robinson",
+    title: "Digital Marketing Analyst, WebFX",
+  },
+  {
+    testimonial: `"Adrian is and has been such a joy to work with and grow alongside at WebFX, back in 2020 when we started as interns until now! She just does it all and does it like no one else... being on-call, bringing STELLAR designs to social media tasks and redesigns, presenting a LNL with so much good information + personality, encouraging others in all they do... I can't express how much being on the same team as her means to me and so many others. Cheers to Adrian as she reaches 3 years this month!"`,
+    name: "Elise Forry",
+    title: "Associate Lead Web Designer, WebFX",
+  },
+];
