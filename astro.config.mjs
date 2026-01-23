1d6e2497fe53e3b54ed40b31e1ae3cc87dd440ef
Adrian Glasmyre <aglasmyre@gmail.com>
2026-01-23 18:50:19 -0500

    remove base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://aglas18.github.io/",
-  base: "/aglasmyre-portfolio/",
+  base: "/",
 
   devToolbar: {
     enabled: false,
