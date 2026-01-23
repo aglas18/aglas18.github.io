d31799190824ad389f0843b9cbfa5aafe06e0cb8
Adrian Glasmyre <aglasmyre@gmail.com>
2026-01-23 18:40:07 -0500

    Configure Astro base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://aglas18.github.io/",
+  base: "/aglasmyre-portfolio/",
+
   devToolbar: {
     enabled: false,
   },
