87f08ee96c4c8b380cc4a683daf22f08d6c417f6
Adrian Glasmyre <aglasmyre@gmail.com>
2026-01-23 21:40:36 -0500

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://aglas18.github.io",
   base: "/",
   output: "static",
-  
+
   devToolbar: {
     enabled: false,
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        // Makes ~/ point to the src directory
+        '~/': `${path.resolve('./src')}/`,
+      },
+    },
   },
 });
